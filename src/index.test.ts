0abb9de39b58ead381f1544d672989bc3f3b58e3
河田陽子 <yoko.kawata@kawadayoukononotobukkukonpyuta.local>
2024-11-28 03:18:01 +0900

    test追加

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,40 @@
-import { test, expect } from '@jest/globals';
+import { testClient } from 'hono/testing'
 import app from "./index";
-import { describe } from 'node:test';
 
-// npx jest
-// test('hello world!', () => {
-// 	expect(1 + 1).toBe(2);
-// });
-
-// https://hono.dev/docs/helpers/testing
 describe('Testing app', () => {
-	it('authorized ip address', () => {
-		const req = new Request('/admin/');
-		const res = app.fetch(req);
-		expect(res.status).toBe(200)
-	})
+  it('authorized ip address', async () => {
+    const client:any = testClient(app)
+    // こっちの書き方はheaderが設定されない。
+    // const res = await client.admin.foo.$get({
+    //     headers: new Headers({'CF-Connecting-IP': '222.229.245.22'}),
+    //     // headers: { 'CF-Connecting-IP': '222.229.245.22' }
+    // })
+    const res = await app.request('/admin/foo', {
+        method: 'GET',
+        headers: new Headers({'CF-Connecting-IP': '222.229.245.22'}),
+    })
+    const status = res.status
+    const body = await res.text()
+    expect(status).toBe(200)
+    expect(body).toBe('authorized page.')
+  })
+
+  it('unauthorized ip address', async () => {
+    const client:any = testClient(app)
+    const res = await app.request('/admin/foo', {
+        method: 'GET',
+        headers: new Headers({'CF-Connecting-IP': '192.168.1.1'}),
+    })
+    const status = res.status
+    const body = await res.text()
+    expect(status).toBe(200)
+    expect(body).toBe('unauthorized page!!')
+  })
+
+  it('root path', async () => {
+    const client:any = testClient(app)
+    const res = await client.$get()
+    const status = res.status
+    expect(status).toBe(200)
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,62 @@
 import { Hono } from 'hono'
 import { getConnInfo } from 'hono/cloudflare-workers'
 import { ConnInfo } from 'hono/conninfo'
+import { ipRestriction } from 'hono/ip-restriction'
 
 const app = new Hono()
 
+// app.use(
+//   '/admin/*',
+//   ipRestriction(
+//     getConnInfo,
+//     {
+//       allowList: ['222.229.245.22'],
+//     },
+//     async (remote, c) => {
+//       return c.text(`Blocking access from ${remote.addr}`, 403)
+//     }
+//   )
+// )
+
 // admin以下のページはip制限をする
 app.get('/admin/*', (c) => {
-  const ip = getConnInfo(c)
+  // const ip = getConnInfo(c)
+  const ip = c.req.header('CF-Connecting-IP')
   if (isAuthorized(ip)) {
-    return c.text("authorized ip address: " + ip.remote.address)
+    return c.text("authorized page.")
   }
   // リダイレクトさせるべきかな
-  return c.text('unauthorized page!! ip address: ' + ip.remote.address )
+  return c.text('unauthorized page!!')
 })
 
 app.get('/', (c) => {
   return c.text('Hello, World!')
 })
 
-function isAuthorized(ip: ConnInfo): boolean {
+function isAuthorized(ip: string | undefined): boolean {
   const allowlist = ['222.229.245.22', '240b:10:2642:5500:803a:8727:a224:6454']
 
-  if (ip.remote.address == undefined) {
+  if (ip == undefined) {
     return false
   }
 
   // ip.remote.addressTypeにはundefinedが設定されるので参照しない
-  return allowlist.includes(ip.remote.address)
+  return allowlist.includes(ip)
 
 }
+
+// function isAuthorized(ip: ConnInfo): boolean {
+//   const allowlist = ['222.229.245.22', '240b:10:2642:5500:803a:8727:a224:6454']
+//   console.log('ppppppppppppp')
+//   console.log(ip)
+//   if (ip.remote.address == undefined) {
+//     return false
+//   }
+
+//   // ip.remote.addressTypeにはundefinedが設定されるので参照しない
+//   return allowlist.includes(ip.remote.address)
+
+// }
 
 
 export default app
